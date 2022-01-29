441c1ca5d2987eaf8b86829d7e5a0ba44fe83d87
tellxmaster <telleriaelias@gmail.com>
2022-01-28 22:32:38 -0500

    Docs creados

---
+++ docs/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";!function(e){console.log("Creando una etiqueta h1...");var n=document.createElement("h1");n.innerText="Hola, ".concat(e," !!!"),document.body.append(n);var o=document.createElement("img");o.src=webpacklogo,document.body.append(o)}("Elias")})();
